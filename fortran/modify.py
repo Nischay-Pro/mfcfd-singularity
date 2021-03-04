6d490068d3641dfdb26db9699fad66ffc0b2fd67
Nischay Ram Mamidi <NischayPro@gmail.com>
2021-03-04 09:57:29 +0530

    gtc

---
+++ fortran/modify.py
@@ -0,0 +1,19 @@
+import subprocess
+
+def main():
+    spack = open("/root/.spack/linux/compilers.yaml").read()
+
+    nvhpc_path = getApplicationOutput(["/opt/spack/bin/spack", "location", "-i", "nvhpc@21.1"]).split("\n")[0]
+
+    data = open("/root/compiler.template").read()
+    data = data.replace("{{PATH}}", nvhpc_path)
+    data = spack + data
+    with open("/root/.spack/linux/compilers.yaml", 'w') as f:
+        f.write(data)
+
+def getApplicationOutput(command, shell=False):
+    return subprocess.run(command, shell=shell, check=True, universal_newlines=True,
+                          stdout=subprocess.PIPE).stdout
+
+if __name__ == "__main__":
+    main()
